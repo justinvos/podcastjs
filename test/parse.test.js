29c683bcc4b3778d87fdd6b0fd514e29ab8a7cae
Erin Dachtler <erin@erindachtler.me>
2020-10-05 21:42:50 -0700

    parse image on podcast

--- test/parse.test.js
+++ test/parse.test.js
@@ -5,6 +5,7 @@ test.concurrent('full fetch test - Hello Internet', async () => {
   const feed = await getFixture('hello_internet_rss.xml')
   const podcast = await podcastJs.parsePodcast(feed)
   expect(podcast.title).toBe('Hello Internet')
+  expect(podcast.image).toBe('https://images.squarespace-cdn.com/content/52d66949e4b0a8cec3bcdd46/1391195775824-JVU9K0BX50LWOKG99BL5/Hello+Internet.003.png?content-type=image%2Fpng')
 })
 
 test.concurrent('full fetch test - Cortex', async () => {
